cac668f8937b034d988a1d08a7b19d050e88629a
Keobeditse <keobiditseboganetswe@gmail.com>
2024-07-29 16:38:30 +0200

    Update python.py

--- python.py
+++ python.py
@@ -2,29 +2,15 @@
 
 import requests
 from bs4 import BeautifulSoup
-import csv
 
-URL = "http://www.values.com/inspirational-quotes"
-r = requests.get(URL)
+def get_local_weather():
+    url = 'https://www.example.com/local-weather'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    weather_info = soup.find('div', class_='weather-info').text
+    return weather_info
 
-soup = BeautifulSoup(r.content, 'html5lib')
+if __name__ == "__main__":
+    weather = get_local_weather()
+    print(f"Local Weather Information: {weather}")
 
-quotes=[] # a list to store quotes
-
-table = soup.find('div', attrs = {'id':'all_quotes'})
-
-for row in table.findAll('div',attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
-	quote = {}
-	quote['theme'] = row.h5.text
-	quote['url'] = row.a['href']
-	quote['img'] = row.img['src']
-	quote['lines'] = row.img['alt'].split(" #")[0]
-	quote['author'] = row.img['alt'].split(" #")[1]
-	quotes.append(quote)
-
-filename = 'inspirational_quotes.csv'
-with open(filename, 'w', newline='') as f:
-	w = csv.DictWriter(f,['theme','url','img','lines','author'])
-	w.writeheader()
-	for quote in quotes:
-		w.writerow(quote)
